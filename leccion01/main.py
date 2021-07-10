5b812e9a4821eef72f37d2a419125061dc93f3d5
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-10 11:09:22 -0300

    Added file operation with python

--- leccion01/main.py
+++ leccion01/main.py
@@ -2,6 +2,7 @@
 # y vamos a poder utilizar tantas veces que lo necesitemos antes que sea destruida o al final de la ejecución de la aplicación
 
 myVariable = "Hello from Python"
+#Print es una función que recibe los argumentos para cmostrar en la consola
 print(myVariable)
 print(myVariable)
 print(myVariable)
