d74f964449a4c82dccb22740ae76aa6b2d875049
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-10 10:59:32 -0300

    First lesson with variables in python

--- leccion01/main.py
+++ leccion01/main.py
@@ -1,1 +1,14 @@
-print("Hello world with python")
+myVariable = "Hello from Python"
+print(myVariable)
+print(myVariable)
+print(myVariable)
+
+# No es necesario crear el tipo de la variable en python, lo hace forma inferencia
+
+myVariableNumber = 2
+print(myVariableNumber)
+print(myVariableNumber)
+print(myVariableNumber)
+
+myVariableNumber = 10
+print(myVariableNumber)
