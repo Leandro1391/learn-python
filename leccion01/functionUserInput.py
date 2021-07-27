7696839bfc7bf8469626861034e4e1006175ea6e
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-26 23:31:00 -0300

    Ejemplo con la funcion input - ingreso de datos a traves de la consola

---
+++ leccion01/functionUserInput.py
@@ -0,0 +1,11 @@
+# Funcion input para procesar la entrada del usuario
+# result = input("Please, Write a message: ")
+# print("Valor proporcionado", result)
+# print("End of program")
+
+number1 = int(input("Write the first number: "))
+number2 = int(input("Write the second number: "))
+result = number1 + number2
+print("The result is:", result)
+
+# La función input devuelve o retorno un tipo Strin(str)
