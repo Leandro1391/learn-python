7b8251ae34ec40b337bc667a283015a21a2a7cb4
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-26 22:51:46 -0300

    Repaso de los tipos de datos basicos en python (int, float, bool, string)

--- leccion01/dataTypes.py
+++ leccion01/dataTypes.py
@@ -1,10 +1,12 @@
+# Tipo int
 x = 10
 print(x)
 # Con type puedo saber que tipo de dato es la variable x
 print(type(x))
 # <class 'int'>
 
-x = 'Hello   World'
+# Tipo string
+x = 'Hello World'
 print(x)
 # <class 'string'>
 print(type(x))
@@ -13,18 +15,24 @@ print(type(x))
 # la palabra reservada hint: no definde el tipo de dato que va alamcenar la variable
 # Con hint = pusta o indicación podemos indicar cual es tipo que apunta la variablre
 
-y: str = 'Hello moto'
+y: str = "Hello moto"
 print(y)
 print(type(y))
 # Aclaración: el hint no define que la variable solamente puede almacenar datos de tipo string, solamente es una INDICACION
 # para saber el tipo de dato que puede estar almacenando,
 # las variables en python son DINAMICAS pueden cambiar
 
+# Type float
 x = 10.5
 print(x)
 print(type(x))
 
+# Type bool
 x = True
+print(x)
+print(type(x))
+
+x = False
 print(x)
 print(type(x))
 
