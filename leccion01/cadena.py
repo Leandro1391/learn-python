600989fcfe6bcea7b1f39fd90474c76d43b4aad6
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-26 23:08:13 -0300

    Conversion de string a int

--- leccion01/cadena.py
+++ leccion01/cadena.py
@@ -1,4 +1,20 @@
 # Cadena (String)
 myFavoriteGroup = "Aerosmith" + " Best Rock Band"
+comment = "In the World"
 # Concatenar con string con el simbolo +
-print("My favorite group is: " + myFavoriteGroup)
+# print("My favorite group is: " + myFavoriteGroup + " " + comment)
+
+# Otra forma para concatenar
+print("My favorite group is:", myFavoriteGroup, comment)
+
+number1 = "1"
+number2 = "2"
+print("Concatenación:", number1 + number2)
+
+# Conversino string to int -> si y solo si el string es un numero
+print("Suma:", int(number1) + int(number2))
+
+# number1 = 1
+# number2 = 2
+# print(number1 + number2)
+
