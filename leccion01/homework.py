a40e42f44f9efdc2607ac45c42e07887cdada753
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-28 23:07:25 -0300

    Completed libro tarea

--- leccion01/homework.py
+++ leccion01/homework.py
@@ -1,11 +1,11 @@
 while True:
-    number = int(input("Como estuvo tu dia (1 al 10): "))
+    number = int(input("Como estuvo tu dia (1 al 10)?: "))
     if not (number >= 1 and number <= 10):
         print("Error, Reingrese un numero del 1 al 10")
     else:
         print("Mi dia estuvo de", number)
         break
-    
+
 # print("Mi dia estuvo de", number)
 
 # print("Mi dia estuvo de:", int(input("Como estuvo tu dia (1 al 10): ")))
---
+++ leccion01/libroTarea.py
@@ -0,0 +1,5 @@
+# title = input("Proporciona el titulo: ")
+# author = input("Proporciona el autor: ")
+# print(title, "fue escrito por", author)
+
+print(input("Proporciona el titulo: "), "fue escrito por", input("Proporciona el autor: "))
