a2b93894db5c2625f70977b3c3e3bb0f24bdcd3f
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-29 22:42:26 -0300

    Agregado ejemplos con todos los operadores en python(suma, resta, multiplicacion, division, resto, division entera y potencia)

---
+++ leccion02/allOperators.py
@@ -0,0 +1,26 @@
+operatorA = 3
+operatorB = 2
+
+# Resta
+minus = operatorA - operatorB
+print(f'Resultado resta: {minus}')
+
+# multiply
+multiplicate = operatorA * operatorB
+print(f'Resultado multiplicación: {multiplicate}')
+
+# Division
+div = operatorA / operatorB
+print(f'Resultado división: {div}')
+
+# division entera
+div = operatorA // operatorB
+print(f'Resultado división: {div}')
+
+# modulo o residuo
+modulo = operatorA % operatorB
+print(f'Resultado residuo división (módulo): {modulo}')
+
+# potencia
+pot = operatorA ** operatorB
+print(f'Resultado potenica de base {operatorA} exponente {operatorB}: {pot}')
