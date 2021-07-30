d2a4bf4b3b3aabd38de6180e143369a3f41a6d0c
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-29 22:56:35 -0300

    Example with reassigment operators

---
+++ leccion02/operatorAssigment.py
@@ -0,0 +1,18 @@
+# operador de asignacion "="
+myVariable = 10
+
+# operadores de reasignación
+myVariable = myVariable + 1
+print(myVariable)
+
+myVariable += 1
+print(myVariable)
+
+myVariable -= 2
+print(myVariable)
+
+myVariable *= 3
+print(myVariable)
+
+myVariable /= 2
+print(myVariable)
