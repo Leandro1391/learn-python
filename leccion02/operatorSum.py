40e3aed3d417c6bcf671a497864ef2adf8001a2d
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-29 22:29:19 -0300

    Agregado ejemplo operador suma

---
+++ leccion02/operatorSum.py
@@ -0,0 +1,8 @@
+operatorA = 3
+operatorB = 2
+sum = operatorA + operatorB
+# print('Resultado suma:', sum)
+
+# lieratal format
+print(f'Resultado suma: {sum}')
+# {} se llama interpolación (incluir variables en una cadena), siempre colocar la letra f al principio
