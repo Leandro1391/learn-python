7c0ed4b5c9ea8080571b0f52ce70c8e97f5eac27
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-29 22:51:12 -0300

    Realizado tarea area y perimetro del triangulo

---
+++ leccion02/triangulo.py
@@ -0,0 +1,5 @@
+altura = int(input('Ingresar altura del triangulo: '))
+base = int(input('Ingresar base del triangulo: '))
+
+print(f'Area: {base * altura}')
+print(f'Perímetro: {(base + altura)*2}')
