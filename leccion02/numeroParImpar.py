a489815eab35ae1c3263f2ac0905379423505a81
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 22:13:22 -0300

    Excercise par o impar

---
+++ leccion02/numeroParImpar.py
@@ -0,0 +1,7 @@
+number = int(input(f'Ingresar un numero para saber si es par o impar '))
+if number % 2 == 0:
+    print(f'El numero {number} es par')
+else:
+    print(f'El numero {number} es impar')
+
+    # operador modulo % es el resto de la division si un numero es divisible por 2 entonces el resto es 0
