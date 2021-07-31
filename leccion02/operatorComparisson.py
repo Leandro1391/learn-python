4bc514d57f3e812536cc5cf6a2ea1cc342c700c7
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 21:55:57 -0300

    Operadores de comparacion

---
+++ leccion02/operatorComparisson.py
@@ -0,0 +1,21 @@
+a = 4
+b = 2
+
+# El lado derecho es lo que se evalua primero
+result = (a == b)
+print(f'Resultado == : {result}')
+
+result = (a != b)
+print(f'Resultado != : {result}')
+
+result = a > b
+print(f'Resultado > : {result}')
+
+result = a >= b
+print(f'Resultado >= : {result}')
+
+result = a < b
+print(f'Resultado < : {result}')
+
+result = a <= b
+print(f'Resultado <= : {result}')
