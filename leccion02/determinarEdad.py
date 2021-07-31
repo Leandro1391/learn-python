2447c80161505de8906e57af73e1143a1169e99c
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 22:16:55 -0300

    Exercise adult age

---
+++ leccion02/determinarEdad.py
@@ -0,0 +1,7 @@
+edadAdulto = 18
+edadPersona = int(input(f'Ingresa tu edad: '))
+
+if edadPersona >= edadAdulto:
+    print(f'La persona es mayor de edad')
+else:
+    print(f'La persona es menor de edad')
