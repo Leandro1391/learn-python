d12b01a2d9d79aacbaaf54a354ac4d0b01a33308
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 22:24:38 -0300

    Excercise logical operators (and, or, not)

---
+++ leccion02/logicalOperators.py
@@ -0,0 +1,14 @@
+a = True
+b = False
+
+# Operador binario
+result = a and b
+print(f'witth and: {result}')
+
+# Operador binario
+result = a or b
+print(f'witth or: {result}')
+
+# El not es un operado unario
+result = not a
+print(f'with not: {result}')
