459a7ecf7053efb397b47c48bec727205cb3ee8c
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 22:04:53 -0300

    Mejorado ejercicio Area y perimetro de un triangulo

--- leccion02/triangulo.py
+++ leccion02/triangulo.py
@@ -1,5 +1,13 @@
-altura = int(input('Ingresar altura del triangulo: '))
-base = int(input('Ingresar base del triangulo: '))
+while True:
+    try:
+        altura = int(input('Ingresar altura del triangulo: '))
+        base = int(input('Ingresar base del triangulo: '))
+        if not (altura > 0 and base > 0):
+            print(f'Los parametros ingresados deben ser mayor a 0')
+        else:
+            break
+    except Exception as e:
+        print(f'Error al ingresar un parametro que no pertenece a los enteros, por favor ingrese un numero entero')
 
 print(f'Area: {base * altura}')
 print(f'Perímetro: {(base + altura)*2}')
