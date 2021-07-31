9872ea3cdd8900754aaf104217c2cc2b1c438eb6
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-07-30 22:29:11 -0300

    Excercise range value

---
+++ leccion02/rangeValue.py
@@ -0,0 +1,5 @@
+number = int(input(f'Ingresar un valor para comprobar si esta dentro del rango del 0 a 5: '))
+if(number >= 0 and number <= 5):
+    print(f'El valor ingresar {number} está dentro del rango')
+else:
+    print(f'El número {number} está fuera del rango')
