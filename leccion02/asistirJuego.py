d272019a88e6bbd87dcb312a9ee87300b8dd422e
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-02 22:44:13 -0300

    With operator not

--- leccion02/asistirJuego.py
+++ leccion02/asistirJuego.py
@@ -1,7 +1,13 @@
-vacaciones = True
+vacaciones = False
 diaDescanso = True
 
-if(vacaciones or diaDescanso):
+if (vacaciones or diaDescanso):
     print(f'Puede asistir al juego de su hijo')
 else:
     print(f'No puede asistir al juego de su hijo')
+
+# With not
+if not (vacaciones or diaDescanso):
+    print(f'No puede asistir al juego de su hijo')
+else:
+    print(f'Puede asistir al juego de su hijo')
