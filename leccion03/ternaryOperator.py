526934dd84b85b67bc25c336856417b60220f655
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-03 21:42:01 -0300

    Example with ternary operator python

---
+++ leccion03/ternaryOperator.py
@@ -0,0 +1,11 @@
+condition = True
+
+# if condition:
+#     print('Condicion verdadera')
+# else:
+#     print('Condicion falsa')
+
+# Solamente usar si el codigo es simple
+# no se puede usar elfi en operadores ternarios
+
+print('Condicion verdadera') if condition else print('Condicion falsa')
