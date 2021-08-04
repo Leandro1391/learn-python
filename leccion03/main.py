e0716ddac1ab96c4be3b240689636ff6001dff54
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-03 21:19:51 -0300

    Example sentence if elif else

---
+++ leccion03/main.py
@@ -0,0 +1,13 @@
+# condition = True
+# condition = 10
+# condition = ''
+condition = 'Hola'
+
+
+# El if tambien valida si la variables contiene un valor vacio-> si está vacio '' es False, en caso contratio es True
+if condition == True:
+    print('Condición verdadera')
+elif condition == False:
+    print('Condición false')
+else:
+    print('Condición no reconocida')
