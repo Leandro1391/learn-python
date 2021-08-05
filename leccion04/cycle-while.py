7c1c6076c26d19c49ff93a0e478458a3fca06290
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-04 22:01:35 -0300

    structure cycle while

---
+++ leccion04/cycle-while.py
@@ -0,0 +1,14 @@
+# condition = True
+
+# while condition:
+#     print('Execute while cycle')
+# else:
+#     print('while cycle end')
+
+cont = 0
+
+while cont < 3:
+    print(cont)
+    cont += 1
+else:
+    print('while cycle end')
