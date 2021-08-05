27a22dbebd777333bb906ae065e7e7602e82c808
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-04 22:18:28 -0300

    Example for cycle python - it's like a foreach

---
+++ leccion04/cycleFor.py
@@ -0,0 +1,10 @@
+# Arreglo es un conjunto de datos
+# iterar es recorrer cada elemento
+
+cadena = 'Hello'
+
+# el for reccorre el string como si fuera una collecion
+for letter in cadena:
+    print(letter)
+else:
+    print('End cycle for')
