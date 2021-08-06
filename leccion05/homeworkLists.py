2c97f61b02a677ff98fe4d19ff2c23b46729f15a
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-05 22:50:28 -0300

    Done homework list divisible between 3

---
+++ leccion05/homeworkLists.py
@@ -0,0 +1,11 @@
+# Iterar un rango de 0 a 10 e imprimir sólo los números divisibles entre 3
+
+# numbers = [0, 1, 2 ,3, 4, 5, 6, 7, 8, 9, 10]
+
+# for number in numbers:
+#     if (number % 3 == 0):
+#         print(number)
+
+for i in range(10):
+    if i % 3 == 0:
+        print(i)
