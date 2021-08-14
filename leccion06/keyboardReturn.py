a8dab6cf73127db89da2f26a55093f126a64f32c
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-08-14 10:05:59 -0300

    Default values and indicadores

--- leccion06/keyboardReturn.py
+++ leccion06/keyboardReturn.py
@@ -1,7 +1,12 @@
-def sum(a, b):
+# Default parameters and with arrow we can indicate the posible type data to return
+def sum(a:int = 0, b:int = 0) -> int:
     return a + b
 
 # result = sum(5, 74)
 # print(f'The result is {result}')
 
 print(f'The result is {sum(70, 10)}')
+
+# Also we can assign default values in the function parameters
+
+# especificar los datos son redundantes en python porque es dinmaíco el comportamiento de la variables pero sirve par aindicar
