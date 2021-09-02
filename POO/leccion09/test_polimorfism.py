e69076c8eef78e82eb751c7174d24e11cdfe10fc
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-09-01 23:17:38 -0300

    isinstance

--- POO/leccion09/test_polimorfism.py
+++ POO/leccion09/test_polimorfism.py
@@ -11,9 +11,15 @@ def print_details(objectTest: Union[Empleado, Gerente]):
     print(objectTest)
     print(type(objectTest))
     objectTest.show_details()
+    if isinstance(objectTest, Gerente):
+        print(objectTest._departamento)
 
 unEmpleado = Empleado('Pedro', 5000)
 print_details(unEmpleado)
 
 unGerente = Gerente('Julieta', 6000, 'Security')
 print_details(unGerente)
+
+# No es recomendable realizar muchas validaciones sobre los tipos en un metodo usando python si es una cuestión muy muy especifica podemos realizar una pequeña validación
+# El metodo tiene que ser lo mas generico posible, tratar en lo posibles aplicar validaciones
+# La narutaleza de puthon es trabajar con objetos en forma dinamica
