28990bb899c8cd5eec624b75412514553d57957a
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-09-01 22:30:36 -0300

    Diferencia en el concecpto de sobrecarga de operadores y sobreescritura de metodos

--- POO/leccion08/overloadOperators.py
+++ POO/leccion08/overloadOperators.py
@@ -1,4 +1,4 @@
-# sobrecarga (overload -> cantidad de argumentos) y sobreescritura (override -> herencia) son conceptos distintos 
+# sobrecarga (overload -> cantidad de argumentos de un mismo metodo en JAVA - Python es distinto) y sobreescritura (override -> herencia) son conceptos distintos 
 # un mismo oeprador puede comportarse de diferentes formas
 # operator +
 
@@ -16,3 +16,8 @@ b = [6,7,8]
 print(a + b)
 
 # myObject1 + myObjetct2
+
+#  La Sobrecarga de un operador significa que el metodo puede comportarse de diferentes formas, nosotros podemos diseniar su comportamiento a nuestra necesidad
+# La Sobreescritura está relacionado con la herencia de clases
+
+# overload operadors and override methods are distinct
