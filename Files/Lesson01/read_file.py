69846ddec85e356edb5958d96cddf2077e2f7efa
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-09-08 22:04:49 -0300

    others ways to manage a file and copy contento in another one

--- Files/Lesson01/read_file.py
+++ Files/Lesson01/read_file.py
@@ -10,8 +10,29 @@ except Exception as e:
 # print(f'{file.read(7)}')
 
 # Lineas comletas
-print(file.readline())
-print(file.readline())
+# print(file.readline())
+# print(file.readline())
 
 # w+:  escribir y leer informacion
 # r+: abre lee y escribi el archivo
+
+# loop lines in the file
+# for line in file:
+#     print(line)
+
+# Read lines - return a list
+# print(file.readlines())
+
+# access one line in the list
+# print(file.readlines()[2])
+
+# opened a new file
+# a - anexa infromacion
+file2 = open('copy.txt', 'a', encoding='utf8')
+file2.write(file.read())
+
+file.close()
+file2.close()
+print('It has ended all the process information')
+
+
