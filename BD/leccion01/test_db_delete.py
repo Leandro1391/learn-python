8bd37f4f1016ca32b1fd0f095640c03175fab7dc
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-09-17 20:20:19 -0300

    Eliminar un registro

---
+++ BD/leccion01/test_db_delete.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+# 1) Create conection object
+conn = psycopg2.connect(
+    user='leandro', 
+    password='',
+    host='localhost',
+    port='5432',
+    database='postgres'
+)
+
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            sentence = 'DELETE FROM test.personas WHERE id_persona=%s'
+            enter = input('Ingresar el id_persona a eliminar: ')
+            values = (enter,)
+            cursor.execute(sentence, values)
+            registros_eliminados = cursor.rowcount
+            print(f'Registros eliminados: {registros_eliminados}')
+            
+except Exception as e:
+    print(f'Exception - Error handled: {e}, {type(e)}')
+finally:
+    conn.close()
