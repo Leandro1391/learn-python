815c434c5c45ca2b7544f037134cbbdf8368b688
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-09-17 18:40:57 -0300

    conection to database postgresql

---
+++ BD/leccion01/test_db.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+# 1) Create conection object
+conn = psycopg2.connect(
+    user='leandro', 
+    password='',
+    host='localhost',
+    port='5432',
+    database='postgres'
+)
+
+print(conn)
+
+# Create cursor -> allow us to execute sql sentences
+
+# 2) Create object cursor for sentences
+cursor = conn.cursor()
+
+sentence = 'SELECT * FROM personas'
+
+# 3) Execute sentence SQL
+cursor.execute(sentence)
+
+# 4) request all register with own sentences execute
+registros = cursor.fetchall()
+
+print(registros)
+
+# Close Cursor
+cursor.close()
+
+# Close conn
+conn.close()
